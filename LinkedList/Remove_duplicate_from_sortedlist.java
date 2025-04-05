c0e921c3067237ce9bccd7be4a537cf9356162c8
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-04-05 22:05:19 +0530

    Update Remove_duplicate_from_sortedlist.java

--- LinkedList/Remove_duplicate_from_sortedlist.java
+++ LinkedList/Remove_duplicate_from_sortedlist.java
@@ -25,19 +25,19 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode deleteDuplicates(ListNode head) {
-//         if (head==null) return null;
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head==null) return null;
 
-//         ListNode current=head;
-//         while(current!=null  &&current.next!=null){
-//             if(current.val==current.next.val){
-//                 current.next=current.next.next;
-//             }
-//             else{
-//                 current=current.next;
-//             }
-//         }
-//         return head;
-//     }
-// }
+        ListNode current=head;
+        while(current!=null  &&current.next!=null){
+            if(current.val==current.next.val){
+                current.next=current.next.next;
+            }
+            else{
+                current=current.next;
+            }
+        }
+        return head;
+    }
+}
