72169e7bbc7e8080c5707923d302ac32380d1d23
Niraj <nirajkumarprajapati32@gmail.com>
2025-05-12 19:43:06 +0530

    commit

---
+++ stack/Easy/Next Greater Element I.java
@@ -0,0 +1,41 @@
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+
+// Code:-
+
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[i]);
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
---
+++ stack/Medium/Next Greater Element II.java
@@ -0,0 +1,39 @@
+// Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+// The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [2,-1,2]
+// Explanation: The first 1's next greater number is 2; 
+// The number 2 can't find next greater number. 
+// The second 1's next greater number needs to search circularly, which is also 2.
+
+
+// Code:-
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int [] result=new int[n];
+        Stack <Integer> stack=new Stack<>();
+
+        Arrays.fill(result,-1);
+
+        for(int i=2*n-1; i>=0;i--){
+            int index=i%n;
+
+            while(!stack.isEmpty() && stack.peek()<=nums[index]){
+                stack.pop();
+            }
+
+            if(i<n){
+                result[index]=stack.isEmpty() ? -1 :stack.peek();
+            }
+            stack.push(nums[index]);
+        }
+        return result;
+    }
+}
