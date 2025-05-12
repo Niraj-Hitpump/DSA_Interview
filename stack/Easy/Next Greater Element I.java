32d4b729f540204af0ed7fc645e260961ac2cff2
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-05-12 19:45:14 +0530

    Update Next Greater Element I.java

--- stack/Easy/Next Greater Element I.java
+++ stack/Easy/Next Greater Element I.java
@@ -14,9 +14,7 @@
 
 
 // Code:-
-
 import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -26,16 +24,13 @@ class Solution {
             while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
                 stack.pop();
             }
-
             map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
             stack.push(nums2[i]);
         }
-
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             result[i] = map.get(nums1[i]);
         }
-
         return result;
     }
 }
