0a0fba19d555599760bf9e955f4b7d45de2e93d0
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-05-24 17:28:04 +0530

    Update Remove Duplicates from Sorted Array.java

--- Array/Easy/Remove Duplicates from Sorted Array.java
+++ Array/Easy/Remove Duplicates from Sorted Array.java
@@ -24,5 +24,5 @@ class Solution {
     }
 }
 
-// Time Complexity: O(n), where n is the length of the array
-// Space Complexity: O(1), since we are modifying the array in place
+// Time Complexity: O(n), where n is the length of the array.
+// Space Complexity: O(1), since we are modifying the array in place.
