e092a1de4df464298cb8351fc1f0107fc7a9cb32
Niraj <nirajkumarprajapati32@gmail.com>
2025-06-15 18:14:09 +0530

    commit

--- Array/Hard/Medianof_two_sortedArray.java
+++ Array/Hard/Medianof_two_sortedArray.java
@@ -15,38 +15,43 @@
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int total = nums1.length + nums2.length;
-        int mid = total / 2;
 
-        int[] mergedArr = mergedArray(nums1, nums2, mid);
-        
-        if (total % 2 != 0) {
+        int total=nums1.length+nums2.length;
+        int mid=total/2;
+        int []mergedArr=mergedArray(nums1,nums2,mid);
+        if(total%2!=0){
+            //odd case
             return (double) mergedArr[mid];
-        } else {
-            return (mergedArr[mid] + mergedArr[mid - 1]) / 2.0;
+        }else{
+            //even case
+            return ((double) mergedArr[mid]+mergedArr[mid-1])/2;
         }
     }
+        public int[] mergedArray(int[] n1,int[] n2,int mid){
+            int [] merged=new int[mid+1];
+            int ptr1=0;
+            int ptr2=0;
+            int ptr3=0;
+            //base case
+            if(n1.length==0) return n2;
+            if(n2.length==0) return n1;
 
-    private int[] mergedArray(int[] n1, int[] n2, int mid) {
-        int[] merged = new int[mid + 1];
-        int ptr1 = 0, ptr2 = 0, ptr3 = 0;
-
-        while (ptr1 < n1.length && ptr2 < n2.length && ptr3 <= mid) {
-            if (n1[ptr1] < n2[ptr2]) {
-                merged[ptr3++] = n1[ptr1++];
-            } else {
-                merged[ptr3++] = n2[ptr2++];
+            while(ptr1<n1.length && ptr2<n2.length && ptr3<=mid){
+                if(n1[ptr1]<n2[ptr2]){
+                    merged[ptr3++]=n1[ptr1++];
+                }else{
+                    merged[ptr3++]=n2[ptr2++];
+                }
             }
-        }
 
-        while (ptr1 < n1.length && ptr3 <= mid) {
-            merged[ptr3++] = n1[ptr1++];
-        }
-
-        while (ptr2 < n2.length && ptr3 <= mid) {
-            merged[ptr3++] = n2[ptr2++];
-        }
-
-        return merged;
+            // for remaining elements
+            while(ptr2<n2.length && ptr3<=mid){
+                merged[ptr3++]=n2[ptr2++];
+            }
+            while(ptr1<n1.length && ptr3<=mid){
+                merged[ptr3++]=n1[ptr1++];
+            }
+            return merged;
+        
     }
 }
