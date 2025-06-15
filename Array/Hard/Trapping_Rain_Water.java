f0200c8bdbeaef73661e080cfc1e431d44169708
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-06-15 22:57:57 +0530

    Update Trapping_Rain_Water.java

--- Array/Hard/Trapping_Rain_Water.java
+++ Array/Hard/Trapping_Rain_Water.java
@@ -6,8 +6,7 @@
 
 // Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
 // In this case, 6 units of rain water (blue section) are being trapped.
-
-// This 
+ 
 // Code:-
 class Solution {
     public int trap(int[] height) {
