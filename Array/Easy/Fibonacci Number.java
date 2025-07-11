0125756d6d83d5b4418d1011ea0dacb6fff3ed42
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-07-11 22:39:02 +0530

    Update Fibonacci Number.java

--- Array/Easy/Fibonacci Number.java
+++ Array/Easy/Fibonacci Number.java
@@ -7,16 +7,16 @@ Given n, calculate F(n).
  
 
 Example 1:
-
 Input: n = 2
 Output: 1
 Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ 
 Example 2:
-
 Input: n = 3
 Output: 2
 Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
 
+ // Iterative approach
 class Solution {
     public int fib(int n) {
         if(n==0) return 0;
@@ -31,7 +31,8 @@ class Solution {
         return b;
     }
 }
-
+recursive appraoch.
+// 
 class Solution {
     public int fib(int n) {
         if (n <= 1) return n;
