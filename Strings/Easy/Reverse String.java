39618add3db57124f81d63a8eb999401e1988260
Niraj <nirajkumarprajapati32@gmail.com>
2025-07-14 23:42:31 +0530

    commit

--- Strings/Easy/Reverse String.java
+++ Strings/Easy/Reverse String.java
@@ -11,16 +11,26 @@
 // Output: ["o","l","l","e","h"]
 
 // code:-
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int left=0; int right=s.length-1;
+//         while(left<right){
+//             char temp=s[right];
+//             s[right]=s[left];
+//             s[left]=temp;
+//             left++;
+//             right--;
+//         }
+//     }
+// }
+
+
+
+
+import java.util.Arrays;
+
 class Solution {
-    public void reverseString(char[] s) {
-        int left=0; int right=s.length-1;
-        while(left<right){
-            char temp=s[right];
-            s[right]=s[left];
-            s[left]=temp;
-            left++;
-            right--;
-        }
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
     }
 }
-
