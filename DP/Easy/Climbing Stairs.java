322d7e20d518ebd584c1a8d9a2d2b205d7e0dddc
Niraj Prajapati <154351209+Niraj-Hitpump@users.noreply.github.com>
2025-07-22 18:52:36 +0530

    Update Climbing Stairs.java

--- DP/Easy/Climbing Stairs.java
+++ DP/Easy/Climbing Stairs.java
@@ -1,10 +1,8 @@
 // Climbing Stairs:-
 
 // You are climbing a staircase. It takes n steps to reach the top.
-
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
- 
 
 // Example 1:
 
@@ -13,8 +11,8 @@
 // Explanation: There are two ways to climb to the top.
 // 1. 1 step + 1 step
 // 2. 2 steps
-// Example 2:
 
+// Example 2:
 // Input: n = 3
 // Output: 3
 // Explanation: There are three ways to climb to the top.
